3bed8a7c47d7deb21fb3a32a5c072838943ef044
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-05-10 14:52:08 +0700

    Try PolygonLayer(square)

--- lib/pages/change_tile.dart
+++ lib/pages/change_tile.dart
@@ -21,6 +21,22 @@ class _Part2ChangeTileState extends State<Part2ChangeTile> {
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
             userAgentPackageName: 'com.example.app',
+          ),
+          PolygonLayer(
+            polygonCulling: false,
+            polygons: [
+              Polygon(
+                  points: [
+                    LatLng(-7.28887331068914, 112.80926395581986),
+                    LatLng(-7.3, 112.80926395581986),
+                    LatLng(-7.3, 113.80926395581986),
+                    LatLng(-7.28887331068914, 113.80926395581986),
+                  ],
+                  color: Colors.blue.withOpacity(0.5),
+                  borderStrokeWidth: 2,
+                  borderColor: Colors.blue,
+                  isFilled: true),
+            ],
           )
         ]);
   }
