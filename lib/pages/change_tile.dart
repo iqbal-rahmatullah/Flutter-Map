998086d18d82f18d9106a87681992f8dea4eb1cb
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-05-10 14:55:27 +0700

    Try circle layer for danger zone

--- lib/pages/change_tile.dart
+++ lib/pages/change_tile.dart
@@ -22,20 +22,17 @@ class _Part2ChangeTileState extends State<Part2ChangeTile> {
             urlTemplate: 'https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
             userAgentPackageName: 'com.example.app',
           ),
-          PolygonLayer(
-            polygonCulling: false,
-            polygons: [
-              Polygon(
-                  points: [
-                    LatLng(-7.28887331068914, 112.80926395581986),
-                    LatLng(-7.3, 112.80926395581986),
-                    LatLng(-7.3, 113.80926395581986),
-                    LatLng(-7.28887331068914, 113.80926395581986),
-                  ],
-                  color: Colors.blue.withOpacity(0.5),
-                  borderStrokeWidth: 2,
-                  borderColor: Colors.blue,
-                  isFilled: true),
+          //
+          CircleLayer(
+            circles: [
+              CircleMarker(
+                point: LatLng(-7.28887331068914, 112.80926395581986),
+                radius: 500,
+                useRadiusInMeter: true,
+                color: Colors.red.withOpacity(0.3),
+                borderColor: Colors.red.withOpacity(0.7),
+                borderStrokeWidth: 2,
+              )
             ],
           )
         ]);
